43ab5b53b6799b45d769115443688452cc81de1f
micmax1000-afk <mic.max.1000@gmail.com>
2026-08-09 22:02:20 +0200

    Add service worker for caching and fetch handling

---
+++ service-worker.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'data-90-giorni-v1';
+const FILES = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
